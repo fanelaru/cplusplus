5727f62a19758d93c4a10b8755db595a7a8c18e0
Stefan <fanelaru@gmail.com>
2019-11-02 07:37:27 +0200

    exemplu template
    template simplu

--- family/main.cpp
+++ family/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
 using namespace std;
+
+template <class fane>
+fane addNo(fane a, fane b) {
+    return (a+b);
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double x=7.65, y=43.43, z;
+    double a=7, b=43, c;
+    z=addNo(x,y);
+    c=addNo(a,b);
+    cout << z << " " << c << endl;
     return 0;
 }
