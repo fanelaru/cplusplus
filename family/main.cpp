b383b9fb33be1c9172277f7b61b24ebc13ceafe2
Stefan <fanelaru@gmail.com>
2019-11-02 07:44:42 +0200

    template cu tip diferit

--- family/main.cpp
+++ family/main.cpp
@@ -6,12 +6,21 @@ fane addNo(fane a, fane b) {
     return (a+b);
 }
 
+template <class PRIMA, class ADOUA>
+PRIMA maimic(PRIMA a, ADOUA b) {
+    return (a<b?a:b);
+}
+
 int main()
 {
     double x=7.65, y=43.43, z;
     double a=7, b=43, c;
+    int m=68;
+    double n=67.45;
     z=addNo(x,y);
     c=addNo(a,b);
     cout << z << " " << c << endl;
+    cout << maimic(m,n) << endl;
+    cout << maimic(n,m) << endl;
     return 0;
 }
