3d7c4a5021c0170c91da259d22d263b07951417c
Stefan <fanelaru@gmail.com>
2019-11-03 22:54:34 +0200

    template specialization
    doar h
    adaugam mai tirziu functionalitate

--- ClassesAndObjects/ClassesAndObjects/Accum.h
+++ ClassesAndObjects/ClassesAndObjects/Accum.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Person.h"
 
 template <class T>
 class Accum
@@ -9,6 +10,18 @@ public:
     Accum(T start): total(start) {};
     Accum<T> operator+=(T const& t) { return total = total + t; };
     T getTotal() const { return total; };
+};
+
+//template specialization
+template <>
+class Accum<Person>
+{
+private:
+    int total;
+public:
+    Accum(int start) : total(start) {};
+    Accum<Person> operator+=(Person const& t) { return total = total + t.getNumber(); };
+    int getTotal() const { return total; };
 };
 
 void testTemplates();
